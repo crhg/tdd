125700e870bed84d3da615d37743844091f2ba3d
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 23:17:38 +0900

    Pairクラス追加

---
+++ src/main/java/org/u17/Pair.java
@@ -0,0 +1,11 @@
+package org.u17;
+
+public class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+}
