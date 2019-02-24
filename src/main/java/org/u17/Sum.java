a3e5765d5622c175b227355dc2c7d3fd03ce23ce
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 23:50:49 +0900

    Sumのtimes実装

--- src/main/java/org/u17/Sum.java
+++ src/main/java/org/u17/Sum.java
@@ -13,6 +13,10 @@ public class Sum implements Expression {
         return new Sum(this, addend);
     }
 
+    Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
+    }
+
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount
                 + addend.reduce(bank, to).amount;
