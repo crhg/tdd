1fab9c259bc48e3d7bb9b4a65f7ef77a0281afd0
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 23:33:24 +0900

    Expressionの内部変数augend, addendをExpressionにする

--- src/main/java/org/u17/Sum.java
+++ src/main/java/org/u17/Sum.java
@@ -1,8 +1,8 @@
 package org.u17;
 
 public class Sum implements Expression {
-    Money augend;
-    Money addend;
+    Expression augend;
+    Expression addend;
 
     Sum(Money augend, Money addend) {
         this.augend = augend;
