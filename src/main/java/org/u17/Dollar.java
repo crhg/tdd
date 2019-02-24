a3046fdcc228c38ae2034eb0c3645ae28588e889
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 20:51:55 +0900

    currency削り忘れ

--- src/main/java/org/u17/Dollar.java
+++ src/main/java/org/u17/Dollar.java
@@ -1,13 +1,10 @@
 package org.u17;
 
 public class Dollar extends Money {
-    private String currency;
-
     Dollar(int amount) {
         this.amount = amount;
         this.currency = "USD";
     }
-
 
     Money times(int multiplier) {
         return new Dollar(amount * multiplier);
