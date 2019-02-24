d274859e921152c5450784d0a27aead06f5772f4
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 17:07:18 +0900

    開始

---
+++ src/main/java/org/u17/App.java
@@ -0,0 +1,13 @@
+package org.u17;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
