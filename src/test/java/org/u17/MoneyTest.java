77c4696d3b1205ba146db0c0c15d48b435c2a528
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 17:38:27 +0900

    fix typo

--- src/test/java/org/u17/MoneyTest.java
+++ src/test/java/org/u17/MoneyTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
-    public void testMltiplication() {
+    public void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
