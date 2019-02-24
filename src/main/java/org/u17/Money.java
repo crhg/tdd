8977e03858d87630317bc5d5614d825c6381d3a0
Manabu Matsui <manabu.matsui@gmail.com>
2019-02-24 23:12:56 +0900

    rate()を使って問い合わせる

--- src/main/java/org/u17/Money.java
+++ src/main/java/org/u17/Money.java
@@ -19,7 +19,7 @@ public class Money implements Expression{
     }
 
     public Money reduce(Bank bank, String to) {
-        int rate = (currency.equals("CHF") && to.equals("USD"))? 2:1;
+        int rate = bank.rate(this.currency, to);
         return new Money(amount / rate, to);
     }
 
